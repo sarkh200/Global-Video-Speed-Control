184d5b03165786bbdcf762dd09e98bf84b6d3ec3
Sadra Saharkhiz <sarkh200@gmail.com>
2024-09-16 19:16:46 -0400

    finished

--- content.js
+++ content.js
@@ -1,1 +1,9 @@
-document.querySelector('video').playbackRate = 3.0;
+chrome.storage.local.get('videoSpeed', function (items) {
+	updateVideoSpeed(items.videoSpeed);
+	chrome.storage.local.remove('videoSpeed');
+});
+function updateVideoSpeed(speed) {
+	if (document.querySelector('video') != null) {
+		document.querySelector('video').playbackRate = speed;
+	}
+}
--- main.js
+++ main.js
@@ -1,27 +1,31 @@
 function changeActiveTabSpeed(speed) {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        var currTab = tabs[0];
-        if (currTab) {
-            chrome.scripting
-                .executeScript({
-                    target: { tabId: currTab.id },
-                    files: ["content.js"],
-                });
-        }
-    });
+	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+		var activeTab = tabs[0];
+		var activeTabId = activeTab.id;
+		if (!activeTab.url?.startsWith("chrome://")) {
+			chrome.storage.local.set({
+				videoSpeed: speed
+			}, function () {
+				chrome.scripting.executeScript({
+					target: { tabId: activeTabId },
+					files: ["content.js"],
+				});
+			});
+		}
+	});
 }
 
 let videoSpeed = 1;
 
 window.onload = function () {
-    document.getElementById("applyButton").onclick = function () {
-        console.log("Apply");
-        changeActiveTabSpeed(videoSpeed);
-    };
-    document.getElementById("speedInput").onchange = function () {
-        let speed = document.getElementById("speedInput").value;
-        videoSpeed = speed;
-        console.log(speed);
-        console.log(videoSpeed);
-    };
+	document.getElementById("applyButton").onclick = function () {
+		console.log("Apply");
+		changeActiveTabSpeed(videoSpeed);
+	};
+	document.getElementById("speedInput").onchange = function () {
+		let speed = document.getElementById("speedInput").value;
+		videoSpeed = speed;
+		console.log(speed);
+		console.log(videoSpeed);
+	};
 }
