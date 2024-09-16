2a000faee156495088ba3b3e52409c579c383e5d
Sadra Saharkhiz <sarkh200@gmail.com>
2024-09-16 19:50:46 -0400

    added more options and a reset button

--- main.js
+++ main.js
@@ -1,31 +1,55 @@
 function changeActiveTabSpeed(speed) {
-	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-		var activeTab = tabs[0];
-		var activeTabId = activeTab.id;
-		if (!activeTab.url?.startsWith("chrome://")) {
-			chrome.storage.local.set({
-				videoSpeed: speed
-			}, function () {
-				chrome.scripting.executeScript({
-					target: { tabId: activeTabId },
-					files: ["content.js"],
+	let filters = {};
+
+	if (allTabs == false) {
+		filters.active = true;
+	}
+	if (allWindows == false) {
+		filters.currentWindow = true;
+	}
+
+	chrome.tabs.query(filters, function (tabs) {
+		tabs.forEach(tab => {
+			console.log(tab.url);
+			var activeTab = tab;
+			var activeTabId = activeTab.id;
+			if (!activeTab.url?.startsWith("chrome://")) {
+				chrome.storage.local.set({
+					videoSpeed: speed
+				}, function () {
+					chrome.scripting.executeScript({
+						target: { tabId: activeTabId },
+						files: ["content.js"],
+					});
 				});
-			});
-		}
+			}
+		});
 	});
 }
 
 let videoSpeed = 1;
+let allTabs = false;
+let allWindows = false;
 
 window.onload = function () {
 	document.getElementById("applyButton").onclick = function () {
-		console.log("Apply");
 		changeActiveTabSpeed(videoSpeed);
+	};
+	document.getElementById("resetButton").onclick = function () {
+		let _allTabs = allTabs;
+		let _allWindows = allWindows;
+		changeActiveTabSpeed(1);
+		allTabs = _allTabs;
+		allWindows = _allWindows;
 	};
 	document.getElementById("speedInput").onchange = function () {
 		let speed = document.getElementById("speedInput").value;
 		videoSpeed = speed;
-		console.log(speed);
-		console.log(videoSpeed);
 	};
+	document.getElementById("allTabs").onchange = function () {
+		allTabs = !allTabs;
+	}
+	document.getElementById("allWindows").onchange = function () {
+		allWindows = !allWindows;
+	}
 }
